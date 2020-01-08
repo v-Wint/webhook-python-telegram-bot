504ba8d94df5545b50ff0f3a742924cf9fe66182
v-Wint <veselyjburunduk@gmail.com>
2020-01-08 22:30:06 +0200

    Initial commit

---
+++ echo_bot_webhook.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3.8
+from requirements import TOKEN, USERNAME #username of pythonanywhere account
+from flask import Flask, request
+from telegram import Bot, Update
+from telegram.ext import CommandHandler, Dispatcher, MessageHandler, Filters
+import json
+
+# handlers
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+	
+app = Flask(__name__)
+def main():
+    bot = Bot(TOKEN)
+    dp = Dispatcher(bot, None, workers=0, use_context=True)
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    bot.delete_webhook()
+    url = f"https://{USERNAME}.pythonanywhere.com/{TOKEN}"
+    bot.set_webhook(url=url)
+    
+
+    @app.route('/' + TOKEN, methods=['POST'])
+    def webhook():
+        json_string = request.stream.read().decode('utf-8')
+        update = Update.de_json(json.loads(json_string), bot)
+        dp.process_update(update)
+        return 'ok', 200
+
+if __name__ == "main":
+    main()
