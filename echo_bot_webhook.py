cccccd205b19d18464dc1ac683005cab8df03b0c
v-Wint <42693472+v-Wint@users.noreply.github.com>
2020-01-09 11:22:32 +0200

    Add comments

--- echo_bot_webhook.py
+++ echo_bot_webhook.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3.8
-from requirements import TOKEN, USERNAME #username of pythonanywhere account
+from requirements import TOKEN, USERNAME # telegram token and username of pythonanywhere account
 from flask import Flask, request
 from telegram import Bot, Update
 from telegram.ext import CommandHandler, Dispatcher, MessageHandler, Filters
@@ -21,18 +21,19 @@ def echo(update, context):
 	
 app = Flask(__name__)
 def main():
+	# add dispatcher
     bot = Bot(TOKEN)
     dp = Dispatcher(bot, None, workers=0, use_context=True)
-
+	# add handlers
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help_))
     dp.add_handler(MessageHandler(Filters.text, echo))
-
+	# start webhook
     bot.delete_webhook()
     url = f"https://{USERNAME}.pythonanywhere.com/{TOKEN}"
     bot.set_webhook(url=url)
     
-
+	# process updates
     @app.route('/' + TOKEN, methods=['POST'])
     def webhook():
         json_string = request.stream.read().decode('utf-8')
@@ -40,5 +41,6 @@ def main():
         dp.process_update(update)
         return 'ok', 200
 
+# make sure you've inserted your app.py name
 if __name__ == "main":
     main()
